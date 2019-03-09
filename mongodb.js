87536966f67d1970dfb487f0c17b855f8cc22f67
azure desktop <aden7x@gmail.com>
2019-03-08 19:40:41 -0800

    test crud in mongodb

--- mongodb.js
+++ mongodb.js
@@ -13,32 +13,20 @@ MongoClient.connect(connectionURL,{useNewUrlParser: true}, (error, client) => {
   }
   const db = client.db(databaseName)
 
-  // db.collection('users').findOne({_id: new ObjectID('5c81e66b608b9a34703766b3')},(error, user) => {
-  //   if(error) {
-  //     return console.log('unable to fetch')
-  //   }
-  //
-  //   console.log(user)
-  // })
+// db.collection('users').deleteMany({
+//   age: 77
+// }).then((result) => {
+//   console.log(result)
+// }).catch((error) => {
+//   console.log(error)
+// })
 
-  // db.collection('users').find({age: 88}).toArray((error, users) => {
-  //   console.log(users)
-  // })
-  // db.collection('users').find({age: 88}).count((error, count) => {
-  //   console.log(count)
-  // })
+db.collection('tasks').deleteOne({
+  description: 'find milk'
+}).then((result) => {
+  console.log(result)
+}).catch((error) => {
+  console.log(error)
+})
 
-  // db.collection('tasks').findOne({_id: new ObjectID('5c81e817110b60519c77a5a3')}, (error, task) => {
-  //   if(error) {
-  //     return console.log('unable to get task')
-  //   }
-  //   console.log(task)
-  // })
-
-  db.collection('tasks').find({completed: false}).toArray( (error, tasks) => {
-    if(error) {
-      return console.log("unable to retrieve tasks")
-    }
-    console.log(tasks)
-  })
 })
