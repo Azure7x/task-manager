62c87b7fd619f3dcd8ad9d5a56a2ee99355d06ff
azure desktop <aden7x@gmail.com>
2019-04-09 16:52:07 -0700

    add env variables

--- src/routers/user.js
+++ src/routers/user.js
@@ -2,14 +2,16 @@ const express = require('express');
 const multer = require('multer');
 const sharp = require('sharp');
 const User = require('../models/user');
-const router = new express.Router();
 const auth = require('../middleware/auth');
+const {sendWelcomeEmail, sendCancelationEmail} = require('../emails/account');
+const router = new express.Router();
 
 router.post('/users', async (req, res) => {
   const user = new User(req.body);
 
   try {
     await user.save();
+    sendWelcomeEmail(user.email, user.name);
     const token = await user.generateAuthToken();
     res.status(201).send({user, token});
   } catch(e) {
@@ -100,8 +102,8 @@ router.delete('/users/me', auth, async (req, res) => {
     // if(!user) {
     //   return res.status(404).send();
     // }
-
     await req.user.remove();
+    sendCancelationEmail(req.user.email, req.user.name);
     res.send(req.user);
   } catch(e) {
     res.status(500).send();
